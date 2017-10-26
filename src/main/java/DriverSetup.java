f86a9876b64b60cae5500670e1468bd7a85b8bd1
Abinash <abinash.shrestha@introcept.co>
2017-10-26 17:46:25 +0545

    update 26/10

--- src/main/java/DriverSetup.java
+++ src/main/java/DriverSetup.java
@@ -1,9 +1,11 @@
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
 public class DriverSetup {
 
-
+    @BeforeClass
     public void setup() throws InterruptedException {
         WebDriver wDriver =new ChromeDriver();
         System.setProperty("","");
