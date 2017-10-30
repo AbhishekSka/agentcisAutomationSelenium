779d0f8c025cb939effff2e112b1f3465ab0be53
Abinash <abinash.shrestha@introcept.co>
2017-10-30 16:23:01 +0545

    Update 30/10

--- src/main/java/Dirvers/DriverSetup.java
+++ src/main/java/Dirvers/DriverSetup.java
@@ -2,17 +2,24 @@ package Dirvers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class DriverSetup {
-
+    WebDriver wDriver;
     @BeforeClass
     public void setup() throws InterruptedException {
-        WebDriver wDriver =new ChromeDriver();
-        System.setProperty("","");
-        wDriver.get("");
+        wDriver =new ChromeDriver();
+        System.setProperty("webdriver.chrome.drive","/Users/introcept/Downloads/dmg/chrome.exe");
+        wDriver.get("https://democis.agentcisapp.com/");
+        wDriver.manage().window().maximize();
         Thread.sleep(1000);
 
 
+    }
+
+    @AfterClass
+    public void driverQuit(){
+        wDriver.quit();
     }
 }
--- src/main/java/Forms/LeadsForm.java
+++ src/main/java/Forms/LeadsForm.java
@@ -1,10 +1,47 @@
 package Forms;
 
+import Dirvers.DriverSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Hello world!
  *
  */
-public class LeadsForm
+public class LeadsForm extends DriverSetup
 {
+    //name
+    By LeadName = By.name("first_name");
+    By LeadLastname = By.name("last_name");
+    By LeadDOB = By.id("");
+
+    //contact
+    By LeadPhone= By.id("phone");
+    By Leademail=By.id("email");
+    By LeadSecondaryMail=By.id("secondary_email");
+    By Leadcontactpreference=By.id("");
+
+    //Address
+    By LeadStreet=By.id("street");
+    By LeadCity=By.id("city");
+    By LeadState = By.id("state");
+    By LeadZip = By.id("zip_code    ");
+    By LeadCountry = By.id("visa_type");
+    By LeadPassport = By.id("");
+
+    //Intrested services
+    By LeadServicesCheck = By.id("");
+
+    //Other details
+    By LeadperfIntake = By.id("");
+    By LeadVisaExpiryDate = By.id("");
+    By LeadVisaType = By.id("");
+
+    //Office Details
+    By LeadsOfficeChoice = By.id("");
+
+    //Comments
+    By LeadsComments = By.id("comment");
+
 
 }
