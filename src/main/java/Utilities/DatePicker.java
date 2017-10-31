86ef7fa8ca9be71afabfb694a52ee4ecc64d6045
Abinash <abinash.shrestha@introcept.co>
2017-10-31 17:57:54 +0545

    Update 31/10

--- src/main/java/Utilities/DatePicker.java
+++ src/main/java/Utilities/DatePicker.java
@@ -1,6 +1,28 @@
 package Utilities;
 
-public class DatePicker {
+import Dirvers.DriverSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class DatePicker extends DriverSetup {
+
+    WebDriver driver;
+    public DatePicker(WebDriver wDriver){
+        this.driver = wDriver;
+
+    }
+
+
+    public void datepick(){
+
+        WebElement RootPicker = (WebElement) driver.findElements(By.id("//div[@id='datepicker-1509442930369']"));
+        WebElement pickYear = RootPicker.findElement(By.className("//div[@class='flatpickr-month']/div/input[@class*='cur-year']"));
+        pickYear.sendKeys("1991");
+        //WebElement pickMonth=RootPicker.findElement(By.);
+
+
+
+        }
 
 }
