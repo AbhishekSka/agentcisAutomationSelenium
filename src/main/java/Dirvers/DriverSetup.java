91c4606c62d445f92e20aa5cf7c5b369ac694d3a
Abinash <abinash.shrestha@introcept.co>
2017-11-01 17:31:33 +0545

    Update 11/01

--- src/main/java/Dirvers/DriverSetup.java
+++ src/main/java/Dirvers/DriverSetup.java
@@ -1,6 +1,8 @@
 package Dirvers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -14,7 +16,11 @@ public class DriverSetup {
         wDriver.get("https://democis.agentcisapp.com/");
         wDriver.manage().window().maximize();
         Thread.sleep(1000);
-
+        //Login
+        WebElement login = wDriver.findElement(By.id("email"));
+        login.sendKeys("avseqskya@gmail.com");
+        WebElement password=wDriver.findElement(By.id("password"));
+        password.sendKeys("test1234");
 
     }
 
