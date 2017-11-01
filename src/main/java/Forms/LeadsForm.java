c870893c9436c5028207878090dc708ac798d821
Abinash <abinash.shrestha@introcept.co>
2017-11-01 17:31:33 +0545

    Update 11/01

--- src/main/java/Forms/LeadsForm.java
+++ src/main/java/Forms/LeadsForm.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -20,7 +22,8 @@ public class LeadsForm extends DriverSetup
     By LeadPhone= By.id("phone");
     By Leademail=By.id("email");
     By LeadSecondaryMail=By.id("secondary_email");
-    By Leadcontactpreference=By.id("");
+
+    By Leadcontactpreference=By.name("//div[@]");
 
     //Address
     By LeadStreet=By.id("street");
@@ -51,12 +54,60 @@ public class LeadsForm extends DriverSetup
         this.wDriver = wDriver;
     }
 
-    public void FillLeads(){
-        //Name
-        WebElement LName = wDriver.findElement(LeadName);
-        LName.sendKeys("");
-        WebElement LlastName = wDriver.findElement(LeadLastname);
-        LName.sendKeys("");
+        public void FillLeads(){
+            //Name
+            WebElement LName = wDriver.findElement(LeadName);
+            LName.sendKeys("");
+            WebElement LlastName = wDriver.findElement(LeadLastname);
+            LName.sendKeys("");
+            WebElement LDOB = wDriver.findElement(LeadDOB);
 
-    }
+
+            //Contacts
+            wDriver.findElement(LeadPhone).sendKeys();
+            wDriver.findElement(Leademail).sendKeys();
+            wDriver.findElement(LeadSecondaryMail).sendKeys();
+
+            List<WebElement> leadpref=wDriver.findElements(Leadcontactpreference);
+            boolean select=leadpref.get(0).isSelected();
+            if (select==true){
+                leadpref.get(1).click();
+            }else{
+                leadpref.get(0).click();
+            }
+
+
+
+            //Address
+            wDriver.findElement(LeadStreet).sendKeys();
+            wDriver.findElement(LeadCity).sendKeys();
+            wDriver.findElement(LeadState).sendKeys();
+            wDriver.findElement(LeadZip).sendKeys();
+            wDriver.findElement(LeadCountry).sendKeys();
+            wDriver.findElement(LeadPassport).sendKeys();
+
+
+            //Interested Services
+            wDriver.findElement(LeadServicesCheck).sendKeys();
+
+            //Other details
+            wDriver.findElement(LeadperfIntake).sendKeys();
+            wDriver.findElement(LeadVisaExpiryDate).sendKeys();
+            wDriver.findElement(LeadVisaType).sendKeys();
+
+            //Office Details
+            wDriver.findElement(LeadsOfficeChoice).sendKeys();
+
+            //Comments
+            wDriver.findElement(LeadsComments).sendKeys();
+
+
+
+
+
+
+
+
+
+        }
 }
