e5debcc7c01c69c40ccbb8380eee2926d2d56772
Abinash <abinash.shrestha@introcept.co>
2017-11-08 16:56:28 +0545

    Update nov 8 4:56

--- src/main/java/Dirvers/DriverSetup.java
+++ src/main/java/Dirvers/DriverSetup.java
@@ -9,7 +9,7 @@ import org.testng.annotations.*;
 import java.util.List;
 import java.util.Set;
 
-public class DriverSetup implements WebDriver {
+public class DriverSetup {
 
 
     WebDriver wDriver;
@@ -20,7 +20,7 @@ public class DriverSetup implements WebDriver {
 
         System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
         //System.setProperty("webdriver.chrome.driver","/Users/introcept/Downloads/dmg/chrome.exe");
-        wDriver =new ChromeDriver();
+        wDriver = new ChromeDriver();
         wDriver.get("https://democis.agentcisapp.com/");
         wDriver.manage().window().maximize();
         Thread.sleep(1000);
@@ -28,7 +28,7 @@ public class DriverSetup implements WebDriver {
         //Login
         WebElement login = wDriver.findElement(By.id("email"));
         login.sendKeys("avseqskya@gmail.com");
-        WebElement password=wDriver.findElement(By.id("password"));
+        WebElement password = wDriver.findElement(By.id("password"));
         password.sendKeys("test1234");
         wDriver.findElement(By.xpath("//button[@class='loginButton']")).click();
         Thread.sleep(1000);
@@ -37,59 +37,8 @@ public class DriverSetup implements WebDriver {
 
 
     @AfterTest(alwaysRun = true)
-    public void driverQuit(){
+    public void driverQuit() {
         wDriver.quit();
     }
 
-    public void get(String s) {
-
-    }
-
-    public String getCurrentUrl() {
-        return null;
-    }
-
-    public String getTitle() {
-        return null;
-    }
-
-    public List<WebElement> findElements(By by) {
-        return null;
-    }
-
-    public WebElement findElement(By by) {
-        return null;
-    }
-
-    public String getPageSource() {
-        return null;
-    }
-
-    public void close() {
-
-    }
-
-    public void quit() {
-
-    }
-
-    public Set<String> getWindowHandles() {
-        return null;
-    }
-
-    public String getWindowHandle() {
-        return null;
-    }
-
-    public TargetLocator switchTo() {
-        return null;
-    }
-
-    public Navigation navigate() {
-        return null;
-    }
-
-    public Options manage() {
-        return null;
-    }
 }
