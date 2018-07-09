e0020f9ca07b48b8dbbb762878ad3b0b75ccdfcc
AbhishekSka <avseqskya@gmail.com>
2018-07-09 17:55:31 +0545

    9July2018

---
+++ src/Feature/Login.feature
@@ -0,0 +1,11 @@
+Feature:
+  Login to the application
+
+  Scenario:
+  Login to site with correct mailid and password
+
+  Given I navigate to Democis agentcis Application
+    And I enter the mailid  as "dummymail1642@gmail.com" and Password as "agentcis"
+    And I click on login button
+    Then I should see the Dashboad overview page
+
---
+++ src/main/java/BDDTests/Login.java
@@ -0,0 +1,12 @@
+package BDDTests;
+
+import cucumber.api.PendingException;
+
+public class Login {
+    public Login() {
+      /*  Given("^I navigate to Democis agentcis Application$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}*/
